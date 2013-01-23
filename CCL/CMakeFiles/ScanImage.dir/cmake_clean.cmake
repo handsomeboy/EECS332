43e97d0d8a2c08304f4e3065dc46ce83fb609871
Josiah Matlack <josiah@cyrus.(none)>
2013-01-22 23:43:16 -0600

    Dir changes

---
+++ CCL/CMakeFiles/ScanImage.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ScanImage.dir/ScanImage.o"
+  "ScanImage.pdb"
+  "ScanImage"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/ScanImage.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
